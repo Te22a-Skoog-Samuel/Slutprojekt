2977a95b9e4e83f06a1ae3bda2c150333a78a0f6
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2024-03-19 10:55:27 +0100

    Första valet

--- Slutprojekt Interaktiv Berättelse/Program.cs
+++ Slutprojekt Interaktiv Berättelse/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿string choice ="";
+
+Console.WriteLine("Det är sommarlov och tillsammans med dina vänner så har ni bestämmt er att gå till den lokala sjön för att bada.");
+Console.WriteLine("Du och dina kompisar Abel, Cain och Noah Söng bestämmer er för att ta bilen till sjön.");
+Console.WriteLine("Ni kommer till en korsning");
+Console.WriteLine("Åker ni vänster eller höger?");
+
+Console.ReadLine();
+
+if (choice == "vänster")
+{
+    
+
+
+
+}
+
